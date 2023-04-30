2e07c641edf4de2a4b01ca576f43f01f8139f85d
AlpayC <alpaycelik91@googlemail.com>
2023-04-30 16:54:33 +0200

    added last two exercieses

--- 23_28_04_23_Conditional_Statements/03_if_else_lev2_2/assets/js/main.js
+++ 23_28_04_23_Conditional_Statements/03_if_else_lev2_2/assets/js/main.js
@@ -1,7 +1,23 @@
-let aqiNum = Number(document.querySelector("#aqislider").value);
-const aqiValue = document.querySelector(".aqivalue");
-const healthValue = document.querySelector(".healthvalue");
-const healthEffect = document.querySelector(".healtheffect");
+function checkAirQuality() {
+  let aqiValue = Number(document.querySelector("#aqislider").value);
+  let aqiNum = document.querySelector(".aqinum");
+  let healthValue = document.querySelector(".healthvalue");
+  let healthEffect = document.querySelector(".healtheffect");
+  let body = document.querySelector(".body");
 
-console.log(aqiNum);
-function checkAirQuality() {}
+  aqiNum.innerHTML = aqiValue;
+
+  if (aqiValue <= 50) {
+    healthValue.innerHTML = "Good";
+    healthEffect.innerHTML = "Little or no risk";
+    body.style.backgroundColor = "#007401";
+  } else if (aqiValue > 50 && aqiValue < 100) {
+    healthValue.innerHTML = "Moderate";
+    healthEffect.innerHTML = "Acceptable quality";
+    body.style.backgroundColor = "#FCD000";
+  } else {
+    healthValue.innerHTML = "Unhealthy for sensitive groups";
+    healthEffect.innerHTML = "Generable publics not likely affected";
+    body.style.backgroundColor = "#fc9800";
+  }
+}
---
+++ 23_28_04_23_Conditional_Statements/04_if_else_lev2_3/assets/js/main.js
@@ -0,0 +1,7 @@
+function calcVal() {
+  let input = Number(document.querySelector("#input").value) - 27;
+  if (input > 27) {
+    console.log("multipliziert: " + input * 2);
+  } else {
+  }
+}
