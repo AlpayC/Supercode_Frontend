c2b681327c0b6df814725096367700a7638266f4
AlpayC <alpaycelik91@googlemail.com>
2023-05-02 19:30:09 +0200

    add resetfunction to delete old span tags

--- 24_02_05_23_Switch/09_ReplaceAll_lev2_3/assets/js/main.js
+++ 24_02_05_23_Switch/09_ReplaceAll_lev2_3/assets/js/main.js
@@ -1,3 +1,8 @@
+function resetArticle() {
+  let article = document.querySelector("article");
+  article.innerHTML = article.innerText;
+}
+
 function searchTxt() {
   // lege die Variable für den Input-Feld fest. Dabei wird lediglich das Value geloggt;
   let searchInput = document.querySelector("#search-input").value;
@@ -8,7 +13,9 @@ function searchTxt() {
   let spanOpen = "<span class=highlight>";
   let spanEnd = "</span>";
 
-  // Nach einem Input wird im article Element der Suchbegriff aus dem Input Feld mit dem Textinhalt aus dem article verglichen und ersetzt, indem der Suchbegriff in dem og. span Tag mit Klasse "umschlossen" wird. Beginnt man die Suche von vorne, werden alle Span Tags vorher entfernt, da die Funktion den gesamten Inhalt des article Elements durch den neu formatierten Text ersetzt.
+  // Nach einem Input wird im article Element der Suchbegriff aus dem Input Feld mit dem Textinhalt aus dem article verglichen und ersetzt, indem der Suchbegriff in dem og. span Tag mit Klasse "umschlossen" wird. Beginnt man die Suche von vorne, werden alle Span Tags vorher durch die Funktion resetArticle() entfernt
+
+  resetArticle();
 
   article.innerHTML = article.innerText.replaceAll(
     searchInput,
