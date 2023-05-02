d4766ef5681ac026fe7eac092c9476966cd1e17b
AlpayC <alpaycelik91@googlemail.com>
2023-05-02 19:20:32 +0200

    added comments

--- 24_02_05_23_Switch/09_ReplaceAll_lev2_3/assets/js/main.js
+++ 24_02_05_23_Switch/09_ReplaceAll_lev2_3/assets/js/main.js
@@ -1,10 +1,14 @@
 function searchTxt() {
+  // lege die Variable für den Input-Feld fest. Dabei wird lediglich das Value geloggt;
   let searchInput = document.querySelector("#search-input").value;
   console.log(searchInput);
-  let article = document.querySelector(".text");
-  console.log(article);
+  // lege die Variable für das article Element fest;
+  let article = document.querySelector("article");
+  // lege die Variablen für das Span Tag fest. Dabei wird direkt die Klasse "highlight" mitgegeben;
   let spanOpen = "<span class=highlight>";
   let spanEnd = "</span>";
+
+  // Nach einem Input wird im article Element der Suchbegriff aus dem Input Feld mit dem Textinhalt aus dem article verglichen und ersetzt, indem der Suchbegriff in dem og. span Tag mit Klasse "umschlossen" wird. Beginnt man die Suche von vorne, werden alle Span Tags vorher entfernt, da die Funktion den gesamten Inhalt des article Elements durch den neu formatierten Text ersetzt.
 
   article.innerHTML = article.innerText.replaceAll(
     searchInput,
